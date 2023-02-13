83a01461b08aa8e3c046ccb47ef935e98134dc1a
Marco Oliveri <marco_oliveri@elmisoftware.com>
2023-02-13 17:54:36 +0100

    first commit

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // Update our state object however appropriate
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      // When state failes it will automatically created an error object
+      state.error = action.error; // It is automatically generated.
+    });
+  },
+});
+
+export const usersReducer = usersSlice.reducer;

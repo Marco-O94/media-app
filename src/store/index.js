b47bccb5fdec6684da7234d63ba9677f1248bbea
Marco Oliveri <marco_oliveri@elmisoftware.com>
2023-02-14 17:41:17 +0100

    .

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
     /* Here we're not creating an array, it says
     Go an look up the reducer path property. It is a string */
   },
   middleware: (getDefaultMiddleware) => {
     // Necessary part to work fine with the store.
-    return getDefaultMiddleware().concat(albumsApi.middleware);
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware);
   },
 });
 // To not repeat the same thing for every api we'll create, we're going to use the function below
@@ -25,3 +29,9 @@ export {
   useAddAlbumMutation,
   useRemoveAlbumMutation,
 } from "./apis/albumsApi";
+
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from "./apis/photosApi";

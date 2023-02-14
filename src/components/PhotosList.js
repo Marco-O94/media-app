e3d32f293ba0bb3fbb9bd6f9cc1cd1c8afb11c13
Marco Oliveri <marco_oliveri@elmisoftware.com>
2023-02-14 17:53:39 +0100

    .

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,13 +1,25 @@
 import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
 import Button from "./Button";
-
+import Skeleton from "./Skeleton";
+import PhotosListItem from "./PhotosListItem";
 const PhotosList = ({ album }) => {
-  useFetchPhotosQuery(album);
+  const { data, isFetching, error } = useFetchPhotosQuery(album);
   const [addPhoto, addPhotoResults] = useAddPhotoMutation();
 
   const handleAddPhoto = () => {
     addPhoto(album);
   };
+
+  let content;
+  if (isFetching) {
+    content = <Skeleton className="h-8 w-8" time={4} />;
+  } else if (error) {
+    content = <div>Error loading photos.</div>;
+  } else {
+    content = data.map((photo) => {
+      return <PhotosListItem key={photo.id} photo={photo} />;
+    });
+  }
 
   return (
     <div>
@@ -16,6 +28,9 @@ const PhotosList = ({ album }) => {
         <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
           + Add Photo
         </Button>
+      </div>
+      <div className="mx-8 flex flex-row flex-wrap justify-center">
+        {content}
       </div>
     </div>
   );
